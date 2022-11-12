92931dbadc9fe07ef7c992ea4f76cbbaa870a837
Raz Levi <razle102030@gmail.com>
2022-11-12 15:34:07 +0200

    add firebase to project

---
+++ common/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBmWXi5wdtZCkoyfzzjzNT7Qpn1GFRNnxQ",
+    authDomain: "blackjack-8784d.firebaseapp.com",
+    projectId: "blackjack-8784d",
+    storageBucket: "blackjack-8784d.appspot.com",
+    messagingSenderId: "42550768474",
+    appId: "1:42550768474:web:a807ceacc9459706837963",
+    measurementId: "G-4B9BXQ85NW"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+export default firebase;
